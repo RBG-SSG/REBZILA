cb3c9175bf65bf8849fc989c58378e7fa76d4fae
RBG-SSG <63781756+RBG-SSG@users.noreply.github.com>
2020-05-04 01:28:41 +0600

    Add files via upload

---
+++ BTS.sh
@@ -0,0 +1,204 @@
+echo "
+███████████████████████████
+█▄─▄─▀█████─▄─▄─█████─▄▄▄▄█
+██─▄─▀█░░████─███░░██▄▄▄▄─█
+▀▄▄▄▄▀▀▄▄▀▀▀▄▄▄▀▀▄▄▀▀▄▄▄▄▄▀
+"
+sleep 1
+echo "PROJECT TERMINAL V 2.0"
+echo "ANTIBAN FILES CREATED BY BTS_MYTHIC"
+sleep 0.5
+echo "SERVER SETUP AND SHELL UPDATED BY BTS_SSG"
+sleep 0.5
+echo "TELEGRAM CHANNEL : BANGLADESH TIGERS SQUAD"
+sleep 0.5
+echo "FIXING CRUSH"
+sleep 0.5
+cd /data/data/com.pubg.krmobile && rm -rf app_crashrecord files && echo " RBG FIX FC " > files && echo " RBG Fix FC " > app_crashrecord
+rm -rf /storage/emulated/0/Android/data/RBG_VIP
+mkdir /storage/emulated/0/Android/data/RBG_VIP
+cp -r /storage/emulated/0/Android/data/com.pubg.krmobile /storage/emulated/0/Android/data/RBG_VIP
+echo "DONE"
+sleep 0.5
+echo " REMOVEING BAD FILES "
+sleep 0.5
+am force-stop com.pubg.krmobile
+touch /data/data/com.pubg.krmobile/code_cache
+chmod 555 /data/data/com.pubg.krmobile/code_cache
+rm -rf /data/data/com.pubg.krmobile/cache/*
+chmod 555 /data/data/com.pubg.krmobile/cache
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/StatEventReportedFlag;
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/GameErrorNoRecords;
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/paks/PufferFileList.json;
+rm -rf /storage/emulated/0/tencent/Midas/Log
+rm -rf /storage/emulated/0/.backups
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/tbslog
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/ProgramBinaryCache
+rm -rf /storage/emulated/0/.backups/com.pubg.krmobile/helpshift/databases
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/cache
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/tbslog
+rm -rf /data/data/com.pubg.krmobile/databases/tdm.db
+rm -rf /data/data/com.pubg.krmobile/databases/beacon_db
+rm -rf /data/data/com.pubg.krmobile/databases/bugly_db_
+rm -rf /data/data/com.pubg.krmobile/databases/config.db
+rm -rf /data/data/com.pubg.krmobile/databases/iMSDK.db
+rm -rf /storage/emulated/0/.backups/com.pubg.krmobile/helpshift/databases/__hs__backup_dao_storage
+rm -rf /storage/emulated/0/tencent/beacon/meta.dat
+rm -rf /storage/emulated/0/MidasOversea/GUID
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/ca-bundle.pem
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/cacheFile.txt
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/login-identifier.txt
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/cache
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/tbslog
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Logs
+touch /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/PufferTmpDir
+touch /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/puffer_temp
+rm -rf /data/media/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/core_patch_0.17.0.11808.pak
+rm -rf /data/media/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/game_patch_0.17.0.11802.pak
+rm -rf /data/media/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/game_patch_0.17.0.11803.pak
+rm -rf /data/media/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/game_patch_0.17.0.11804.pak
+rm -rf /data/media/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/game_patch_0.17.0.11805.pak
+rm -rf /data/media/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/game_patch_0.17.0.11806.pak
+rm -rf /data/media/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/game_patch_0.17.0.11807.pak
+chmod -R 544 /data/media/0/Android/data/com.pubg.krmobile
+chmod -R 544 /data/media/0/Android/data/com.pubg.krmobile/*
+chmod -R 544 /data/media/0/Android/data/com.pubg.krmobile/*/*
+chmod -R 544 /data/media/0/Android/data/com.pubg.krmobile/*/*/*
+chmod -R 544 /data/media/0/Android/data/com.pubg.krmobile/*/*/*/*
+chmod -R 544 /data/media/0/Android/data/com.pubg.krmobile/*/*/*/*/*
+chmod -R 544 /data/media/0/Android/data/com.pubg.krmobile/*/*/*/*/*/*
+chmod -R 544 /data/media/0/Android/data/com.pubg.krmobile/*/*/*/*/*/*/*
+chmod -R 550 /data/media/0/Android/data/com.pubg.krmobile/files
+chmod -R 664 /data/media/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/SaveGames/*
+chmod -R 444 /data/media/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/*
+chmod -R 444 /data/media/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/SrcVersion.ini
+chmod 755 /data/data/com.pubg.krmobile/lib/*
+echo "INJECTING NEW ANTIBAN"
+cp -r /data/data/com.termux/files/home/REBZILA/Paks /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved
+sleep 1
+echo "STARTING PUBG KOREA "
+am start -n com.pubg.krmobile/com.epicgames.ue4.SplashActivity
+sleep 15
+echo "done"
+sleep 1
+for i in {1..68}
+do
+sleep 68
+done
+echo "INJECTING ANTIBAN AGAIN "
+cp -r /data/data/com.termux/files/home/REBZILA/Paks /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved
+sleep 1
+rm -rf /data/data/com.pubg.krmobile/lib/libcubehawk.so
+rm -rf /data/data/com.pubg.krmobile/lib/libTDataMaster.so
+rm -rf /data/data/com.pubg.krmobile/lib/libtersafe.so
+rm -rf /data/data/com.pubg.krmobile/lib/libzip.so
+rm -rf /data/data/com.pubg.krmobile/lib/libzlib.so
+dd if=/dev/urandom of=/data/data/com.pubg.krmobile/lib/libcubehawk.so bs=2000 count=7075  &> /dev/null
+dd if=/dev/urandom of=/data/data/com.pubg.krmobile/lib/libTDataMaster.so bs=2100 count=9067  &> /dev/null
+dd if=/dev/urandom of=/data/data/com.pubg.krmobile/lib/libtersafe.so bs=2200 count=9986  &> /dev/null
+dd if=/dev/urandom of=/data/data/com.pubg.krmobile/lib/libzip.so bs=2300 count=8977  &> /dev/null
+dd if=/dev/urandom of=/data/data/com.pubg.krmobile/lib/libzlib.so bs=2400 count=7899  &> /dev/null
+chmod -R 2777 /data/data/com.pubg.krmobile/*
+chmod -R 2755 /data/data/com.pubg.krmobile/lib/*
+sleep 1
+Remove () {
+echo " LOG CLEANING "
+iptables -I INPUT -s astat.bugly.qcloud.com -j DROP &>/dev/null
+iptables -I INPUT -s hk.clientreport.gfm.qq.com -j DROP &>/dev/null
+iptables -I INPUT -s gfm.qq.com -j DROP &>/dev/null
+iptables -I INPUT -s csoversea.mbgame.gamesafe.qq.com -j DROP &>/dev/null
+iptables -I INPUT -s vmp.qq.com -j DROP &>/dev/null
+iptables -I INPUT -s pingma.qq.com -j DROP &>/dev/null
+iptables -I INPUT -s 183.61.41.148 -j DROP &>/dev/null;
+iptables -I INPUT -s 49.51.42.152 -j DROP &>/dev/null;
+iptables -I INPUT -s 164.52.10.252 -j DROP &>/dev/null;
+iptables -I INPUT -s sandbox.api.unipay.qq.com -j DROP &>/dev/null;
+iptables -I INPUT -s pay.igamecj.com -j DROP &>/dev/null;
+iptables -I INPUT -s cgipayzlby.gm99.com -j DROP &>/dev/null;
+sleep 5
+echo "━━━━━━━━━━━━━━━"
+echo "▉▉ 30%"
+echo "━━━━━━━━━━━━━━━"
+sleep 1
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/StatEventReportedFlag
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/GameErrorNoRecords
+rm -rf /storage/emulated/0/tencent/Midas/Log
+rm -rf /storage/emulated/0/.backups
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/tbslog
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/ProgramBinaryCache
+rm -rf /storage/emulated/0/.backups/com.pubg.krmobile/helpshift/databases
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/cache
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/tbslog
+echo "Level 1 ✔"
+echo "━━━━━━━━━━━━━━━"
+echo "▉▉▉▉▉▉ 60%"
+echo "━━━━━━━━━━━━━━━"
+sleep 1
+rm -rf /data/data/com.pubg.krmobile/databases/tdm.db
+rm -rf /data/data/com.pubg.krmobile/databases/beacon_db
+rm -rf /data/data/com.pubg.krmobile/databases/bugly_db_
+rm -rf /data/data/com.pubg.krmobile/databases/config.db
+rm -rf /data/data/com.pubg.krmobile/databases/iMSDK.db
+echo "Level 2 ✔"
+echo "━━━━━━━━━━━━━━━"
+echo "▉▉▉▉▉▉▉▉▉▉▉ 100%"
+echo "━━━━━━━━━━━━━━━"
+sleep 1
+echo "Level 3 ✔"
+rm -rf /storage/emulated/0/.backups/com.pubg.krmobile/helpshift/databases/__hs__backup_dao_storage
+rm -rf /storage/emulated/0/tencent/beacon/meta.dat
+rm -rf /storage/emulated/0/MidasOversea/GUID
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/ca-bundle.pem
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/cacheFile.txt
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/login-identifier.txt
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/cache
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/tbslog
+rm -rf /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Logs
+touch /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/PufferTmpDir
+touch /storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/puffer_temp
+printf "BTS LOG CLEANING COUNT→$COUNT❀
+";
+printf "Real Time: ";
+date
+printf "Clearing Logs Automatically
+";
+let COUNT=COUNT+1
+}
+PACKAGE='com.pubg.krmobile'
+while [ $(pidof $PACKAGE) ]
+do
+Remove
+((COUNT=$COUNT+10))
+if [ ! $(pidof $PACKAGE) ]; then
+break
+fi
+sleep 5
+done
+((final=$COUNT/60))
+printf "PUBGM RUNTIME : $final MINUTE ☑"
+printf " "
+echo "RESTORING DATA.."
+cp -r /storage/emulated/0/Android/data/RBG_VIP/com.pubg.krmobile /storage/emulated/0/Android/data
+sleep 1
+su -c chmod 777 /data/data/com.pubg.krmobile/  &> /dev/null
+su -c chmod 777 /data/data/com.pubg.krmobile/*  &> /dev/null
+su -c chmod 777 /data/data/com.pubg.krmobile/*/  &> /dev/null
+su -c chmod 777 /data/data/com.pubg.krmobile/*/*  &> /dev/null
+su -c chmod 777 /data/data/com.pubg.krmobile/*/*/*  &> /dev/null
+su -c chmod 777 /data/data/com.pubg.krmobile/*/*/*/*  &> /dev/null
+su -c chmod 777 /data/data/com.pubg.krmobile/*/*/*/*/*  &> /dev/null
+echo "❀━━━━━━━━━━━━❖━━━━━━━━━━━━❀"
+echo "Processing.."
+echo "❀━━━━━━━━━━━━❖━━━━━━━━━━━━❀"
+sleep 1
+rm -rf /storage/emulated/0/Android/data/RBG_VIP
+pm install -r /data/app/com.pubg.krmobile*/base.apk
+date
+echo "PROJECT BROUGHT TO U BY ...."
+echo "RAINBOW BUTTERFLY GAMING"
+sleep 0.5
+echo "TELEGRAM CHANNEL @RainbowButterflyGaming"
+sleep 0.5
+echo "PROJECT TERMINAL V1.5 FOR ONLY VVIP MEMBERS"
+sleep 1
+am force-stop com.termux
